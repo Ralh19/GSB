c288b2044818340422c439a52fb3e5214e01f76f
Ralh19 <ruben.almeidaalhada@gmail.com>
2024-10-16 16:34:52 +0200

    Connexion + hash fonctionnel, sauf lors d'une erreur de mdp Visiteur2

--- src/Vues/v_accueilcomptable.php
+++ src/Vues/v_accueilcomptable.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Vue Accueil
  *
@@ -14,16 +13,15 @@
  * @version   GIT: <0>
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
-
 ?>
 <div class="alert alert-warning" role="alert"><strong>Rappel : </strong>Vos frais sont à déclarer au plus tard le dernier jour du mois 
-	et vos factures acquittées doivent être arrivées aux services comptables au plus tard le 10 du mois suivant la saisie.
-	Les éléments reçus après le 10 seront reportés sur le mois suivant.
+    et vos factures acquittées doivent être arrivées aux services comptables au plus tard le 10 du mois suivant la saisie.
+    Les éléments reçus après le 10 seront reportés sur le mois suivant.
 </div>
 <div id="accueil">
     <h2>
         Gestion des frais<small> - Comptable : 
-            <?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></small>
+<?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></small>
     </h2>
 </div>
 <div class="row">
@@ -39,7 +37,7 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-12">
                         <a href="index.php?uc=gererFrais&action=saisirFrais"
-                           class="btn btn-danger btn-lg" role="button">
+                           class="btn btn-warning btn-lg" role="button">
                             <span class="glyphicon glyphicon-ok"></span>
                             <br>Valider fiche de frais </a>
                         <a href="index.php?uc=etatFrais&action=selectionnerMois"
